3e33a009e772f2a790060173b402178b06865ce8
Ajay Agarwal <inboxajay@outlook.com>
2018-05-19 17:27:16 -0400

    Test Commit

--- src/main/java/com/aces/learn/spring/docker/SpringDockerApplication.java
+++ src/main/java/com/aces/learn/spring/docker/SpringDockerApplication.java
@@ -8,5 +8,6 @@ public class SpringDockerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDockerApplication.class, args);
+		//System.out.println("Hello");
 	}
 }
