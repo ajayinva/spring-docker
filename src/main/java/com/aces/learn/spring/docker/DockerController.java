d5029c923f7fa843d1c1ec0ac3730b1dcc0743a4
Ajay Agarwal <ajay.agarwal@resonate.com>
2018-05-24 15:34:05 -0400

    asdadadasdsda

--- src/main/java/com/aces/learn/spring/docker/DockerController.java
+++ src/main/java/com/aces/learn/spring/docker/DockerController.java
@@ -18,6 +18,6 @@ public class DockerController {
 	 */
 	@RequestMapping("/sayHello")
 	public String sayHello(){		
-		return "Hello World from Docker Edited2";
+		return "I am a change in the Master Branch";
 	}
 }
